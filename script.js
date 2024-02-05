0270e018d92fcacaa9db774cf98f08b4c8f164c4
Samuel <samuferfort@gmail.com>
2024-02-05 03:34:34 +0100

    Added UI to rps game instead of using prompt

--- script.js
+++ script.js
@@ -1,56 +1,79 @@
 
-
 let result = "";
- let playerVictory = 0;
- let computerVictory = 0;
- let computerMove = 0;
- let playerMove;
- console.log("Best of 5!");
+let playerVictory = 0;
+let computerVictory = 0;
+let computerMove = "";
+const displayWinner = document.querySelector("#section");
+const p = document.createElement("p");
+const displayResult = document.createElement("p");
 
 
 function getComputerChoice() {
     let computerMove = Math.ceil(Math.random() * 3);
     if (computerMove === 1) {
-        computerMove = "rock";
-            
+        computerMove = "rock";         
     }
     else if (computerMove === 2) {
         computerMove = "paper";  
     }
     else {
-        computerMove = "scissors"    
+        computerMove = "scissors";    
     }
-    console.log(`The computer move is: ${computerMove}`);
     return computerMove;
 }
+
+
 function playRound(playerMove, computerMove){
-    playerMove = prompt("Whats your move? ");
-    playerMove = playerMove.toLowerCase();
-    computerMove = getComputerChoice();
-    if (playerMove === computerMove){
-        result = "Draw!";
-    } else if (
-        (playerMove === "rock" && computerMove === "scissors")|| 
-        (playerMove === "paper" && computerMove === "rock") || 
-        (playerMove === "scissors" && computerMove === "paper")
-    ){
-        result = `You win! ${playerMove} beats ${computerMove}`
-        playerVictory++;
-    } else {
-        result = `You win! ${computerMove} beats ${playerMove}`
-        computerVictory++
+    if ((playerVictory + computerVictory) < 5) {
+        playerMove = playerMove.toLowerCase();
+        computerMove = getComputerChoice();
+
+
+        if (playerMove === computerMove){
+            result = `Draw!`;
+            
+        } else if (
+            (playerMove === "rock" && computerMove === "scissors")|| 
+            (playerMove === "paper" && computerMove === "rock") || 
+            (playerMove === "scissors" && computerMove === "paper")
+        ){
+            result = `You win! ${playerMove} beats ${computerMove}`;
+            playerVictory++;
+        } else {
+            result = `You lose! ${computerMove} beats ${playerMove}`;
+            computerVictory++;
+        }
+        
+        
+        const computerMoveDisplay = document.createElement("p");
+        computerMoveDisplay.innerText = `The computer move is ${computerMove}`
+        displayWinner.appendChild(computerMoveDisplay)
+        
+
+        setTimeout(() => {
+            const resultDisplay = document.createElement("p");
+            resultDisplay.innerText = result;
+            displayWinner.appendChild(resultDisplay);
+        }, 500);
+        console.log(computerVictory);
+    }else {
+        displayWinnerResult();
     }
-    console.log(result);
 }
-for (let i = 0; i < 5; i++) {
-    playRound(playerMove, computerMove);
+
+
+function displayWinnerResult() {
+        if (playerVictory > computerVictory) {
+            displayResult.innerHTML = `You win ${playerVictory} to ${computerVictory}`;
+        } else {
+            displayResult.innerHTML = `You lose ${computerVictory} to ${playerVictory}`;
+        }
+        document.body.appendChild(displayResult);
 }
-if (playerVictory > computerVictory) {
-    console.log(`You won the overall game!`)
-}else {
-    console.log(`You lost! The computer reach 5 victories first`)
-}
+
+
+const move = document.querySelectorAll(".playerMove");
+
+move.forEach( element => element.addEventListener("click", () => playRound(element.innerHTML, computerMove)));
+
     
-
-
-
